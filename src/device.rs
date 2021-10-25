83247c610486dac36e6a333b5cc18ac567729baa
lz1998 <875543533@qq.com>
2021-10-26 00:11:04 +0800

    device info

--- src/device.rs
+++ src/device.rs
@@ -25,10 +25,51 @@ pub struct DeviceInfo {
     pub guid: Vec<u8>,
 }
 
+impl DeviceInfo {
+    pub fn random() -> DeviceInfo {
+        DeviceInfo {
+            display: "".to_string(),
+            product: "".to_string(),
+            device: "".to_string(),
+            board: "".to_string(),
+            model: "".to_string(),
+            finger_print: "".to_string(),
+            imei: "".to_string(),
+            brand: "".to_string(),
+            bootloader: "".to_string(),
+            base_band: "".to_string(),
+            version: Version::new(),
+            sim_info: "".to_string(),
+            os_type: "".to_string(),
+            mac_address: "".to_string(),
+            ip_address: vec![],
+            wifi_bssid: "".to_string(),
+            wifi_ssid: "".to_string(),
+            imsi_md5: vec![],
+            android_id: "".to_string(),
+            apn: "".to_string(),
+            vendor_name: "".to_string(),
+            vendor_os_name: "".to_string(),
+            guid: vec![],
+        }
+    }
+}
+
 //系统版本
 pub struct Version {
     pub incremental: String,
     pub release: String,
     pub codename: String,
     pub sdk: u32,
+}
+
+impl Version {
+    pub fn new() -> Version {
+        Version {
+            incremental: "5891938".to_string(),
+            release: "10".to_string(),
+            codename: "REL".to_string(),
+            sdk: 29,
+        }
+    }
 }
