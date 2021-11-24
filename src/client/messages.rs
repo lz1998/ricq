2493819d6781fbead840e9c10b709407d4feb5be
abrahumlink <307887491@qq.com>
2021-11-24 21:37:26 +0800

    🚧 message element

--- src/client/messages.rs
+++ src/client/messages.rs
@@ -1,4 +1,7 @@
+use bytes::{Buf, Bytes};
+
 use crate::client::income::decoder::online_push::GroupMessagePart;
+use crate::pb::msg::Ptt;
 use std::ops::{Add, AddAssign};
 
 #[derive(Debug, Clone, PartialEq)]
@@ -39,18 +42,117 @@ pub struct AnonymousInfo {
     pub anonymous_nick: String,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum MsgElement {
-    // todo copilot write this...
-    Text(String),
-    Image(String),
-    Audio(String),
-    Video(String),
-    File(String),
-    Face(String),
-    Location(String),
-    RichText(String),
-    Unknown(String),
+    Reply {
+        reply_seq: i32,
+        sender: i64,
+        group_id: i64,
+        time: i32,
+        elements: Vec<MsgElement>,
+    },
+    GroupFile {
+        name: String,
+        size: i64,
+        path: String,
+        busid: i32,
+    },
+    Text {
+        content: String,
+    },
+    Voice {
+        name: String,
+        md5: [u8; 16],
+        size: i32,
+        url: String,
+        data: Bytes, // sending
+    },
+    GroupVoice {
+        data: Bytes,
+        ptt: Ptt,
+    },
+    PrivateVoice {
+        date: Bytes,
+        ptt: Ptt,
+    },
+    Face {
+        index: i32,
+        name: String,
+    },
+    At {
+        target: i64,
+        display: String,
+        guild: bool,
+    },
+    ShortVideo {
+        name: String,
+        uuid: Bytes,
+        size: i32,
+        thumb_size: i32,
+        md5: Bytes,       // [u8;16]
+        thumb_md5: Bytes, // [u8;16]
+        utl: String,
+        guild: bool,
+    },
+    Service {
+        id: i32,
+        content: String,
+        res_id: String,
+        sub_type: String,
+    },
+    LightApp {
+        content: String,
+    },
+    RedBag {
+        red_bag_type: RedBagType,
+        title: String,
+    },
+    Music {
+        music_type: MusicType,
+        title: String,
+        brief: String,
+        summary: String,
+        url: String,
+        picture_url: String,
+        music_url: String,
+    },
+    AnimatedSticker {
+        id: i32,
+        name: String,
+    },
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum MusicType {
+    QQMusic,    // QQ音乐
+    CloudMusic, // 网易云音乐
+    MiguMusic,  // 咪咕音乐
+    KugouMusic, // 酷狗音乐
+    KuwoMusic,  // 酷我音乐
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum RedBagType {
+    RedBagSimple,
+    RedBagLucky,
+    RedBagSimpleTheme,
+    RedBagLuckyTheme,
+    RedBagWord,
+    RedBagSimpleSpecify,
+    RedBagLuckySpecify,
+    RedBagSimpleSpecifyOver3,
+    RedBagLuckySpecifyOver3,
+    RedBagVoice,
+    RedBagLook, // ?
+    RedBagVoiceC2C,
+    RedBagH5,
+    RedBagKSong,
+    RedBagEmoji,
+    RedBagDraw,
+    RedBagH5Common,
+    RedBagWordChain,
+    RedBagKeyword,        // ?
+    RedBagDrawMultiModel, // ??
 }
 
 impl Add<Vec<MsgElement>> for MsgElement {
@@ -85,6 +187,38 @@ impl GroupMessage {
     }
 }
 
-pub fn parse_msg_elements(input: Vec<crate::pb::msg::Elem>) -> Vec<MsgElement> {
-    todo!()
+pub fn parse_msg_elements(elems: Vec<crate::pb::msg::Elem>) -> Vec<MsgElement> {
+    let mut msgs = vec![];
+    for elem in elems {
+        if let Some(Some(m)) = elem.src_msg.map(|m| {
+            if m.orig_seqs.len() == 0 {
+                None
+            } else {
+                Some(m)
+            }
+        }) {
+            // 回复消息
+            msgs.push(MsgElement::Reply {
+                reply_seq: m.orig_seqs[0],
+                time: m.time(),
+                sender: m.sender_uin(),
+                group_id: m.to_uin(),
+                elements: parse_msg_elements(m.elems.clone()),
+            });
+        }
+
+        if let Some(info) = elem.trans_elem_info {
+            if info.elem_type() == 24 {
+                let mut bytes = Bytes::copy_from_slice(info.elem_value());
+                let i3 = bytes.len();
+                if i3 > 3 {
+                    if bytes.get_u8() == 1 {
+                        // todo
+                    }
+                }
+            }
+        }
+    }
+    todo!();
+    return msgs;
 }
