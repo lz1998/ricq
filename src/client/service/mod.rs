3c197eb0b320c2f0d35270d2680ce3520de54747
lz1998 <875543533@qq.com>
2022-01-25 23:07:12 +0800

    service

--- src/client/service/mod.rs
+++ src/client/service/mod.rs
@@ -1,3 +1,4 @@
+use std::any::TypeId;
 use std::convert::Infallible;
 use std::fmt;
 use std::future::Future;
@@ -8,83 +9,79 @@ use std::task::{Context, Poll};
 use async_trait::async_trait;
 use futures::FutureExt;
 use tower::util::BoxCloneService;
-use tower::Service;
+use tower::{Service, ServiceBuilder};
+
+use rq_engine::{GroupMessageEvent, PrivateMessageEvent};
 
 pub struct Body;
 pub struct Request<B>(B);
 pub type Response = ();
 
-#[async_trait]
-pub trait Handler<T, B = Body>: Clone + Send + Sized + 'static {
-    async fn call(self, req: Request<B>) -> ();
-}
-
-pub struct IntoService<H, T, B> {
-    handler: H,
-    _marker: PhantomData<fn() -> (T, B)>,
-}
-
-impl<H, T, B> IntoService<H, T, B> {
-    pub(super) fn new(handler: H) -> Self {
-        Self {
-            handler,
-            _marker: PhantomData,
-        }
-    }
-}
-
-impl<H, T, B> fmt::Debug for IntoService<H, T, B> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_tuple("IntoService")
-            .field(&format_args!("..."))
-            .finish()
-    }
-}
-
-impl<H, T, B> Clone for IntoService<H, T, B>
+pub fn new_service<F, Fut, E>(f: F) -> BoxCloneService<E, (), Infallible>
 where
-    H: Clone,
+    F: FnOnce(E) -> Fut + Copy + Send + 'static + Sync,
+    Fut: Future<Output = ()> + Send,
+    E: Send + 'static,
 {
-    fn clone(&self) -> Self {
-        Self {
-            handler: self.handler.clone(),
-            _marker: PhantomData,
-        }
-    }
+    let service = ServiceBuilder::new().service_fn(move |req| async move {
+        f(req).await;
+        Ok(())
+    });
+    BoxCloneService::new(service)
 }
 
-impl<H, T, B> Service<Request<B>> for IntoService<H, T, B>
-where
-    H: Handler<T, B> + Clone + Send + 'static,
-    B: Send + 'static,
-{
-    type Response = ();
-    type Error = Infallible;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
-
-    #[inline]
-    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        Poll::Ready(Ok(()))
-    }
-
-    fn call(&mut self, req: Request<B>) -> Self::Future {
-        let handler = self.handler.clone();
-        let future = async move {
-            Handler::call(handler, req).await;
-            Ok(())
-        };
-        Box::pin(future)
-    }
+pub struct Handlers {
+    pub private_message_handlers: Vec<BoxCloneService<PrivateMessageEvent, (), Infallible>>,
+    pub group_message_handlers: Vec<BoxCloneService<GroupMessageEvent, (), Infallible>>,
 }
 
-pub struct Route<B = Body, E = Infallible>(pub(crate) BoxCloneService<Request<B>, Response, E>);
-
-impl<B, E> Route<B, E> {
-    pub(super) fn new<T>(svc: T) -> Self
+pub trait AddHandler<E> {
+    fn add_handler<F, Fut>(&mut self, f: F)
     where
-        T: Service<Request<B>, Response = (), Error = E> + Clone + Send + 'static,
-        T::Future: Send + 'static,
+        F: FnOnce(E) -> Fut + Copy + Send + 'static + Sync,
+        Fut: Future<Output = ()> + Send;
+    // E: Send + 'static;
+}
+
+impl AddHandler<GroupMessageEvent> for Handlers {
+    fn add_handler<F, Fut>(&mut self, f: F)
+    where
+        F: FnOnce(GroupMessageEvent) -> Fut + Copy + Send + 'static + Sync,
+        Fut: Future<Output = ()> + Send,
+        GroupMessageEvent: Send + 'static,
     {
-        Self(BoxCloneService::new(svc))
+        // TypeId::of::<E>();
+        let service = ServiceBuilder::new().service_fn(move |req| async move {
+            f(req).await;
+            Ok(())
+        });
+        let service = BoxCloneService::new(service);
+        self.group_message_handlers.push(service);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test() {
+        let mut router = Handlers {
+            private_message_handlers: vec![],
+            group_message_handlers: vec![],
+        };
+        router.add_handler(process_group1);
+        router.add_handler(process_group2);
+        let g = GroupMessageEvent::default();
+        for mut r in router.group_message_handlers {
+            r.call(g.clone()).await;
+        }
+    }
+
+    async fn process_group1(e: GroupMessageEvent) {
+        println!("1: {:?}", e)
+    }
+    async fn process_group2(e: GroupMessageEvent) {
+        println!("2: {:?}", e)
     }
 }
