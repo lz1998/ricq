fc9154cc464c2fdb438f21d1960d3d574b66292a
lz1998 <875543533@qq.com>
2022-01-27 23:26:01 +0800

    pub use structs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ pub use client::Client;
 pub use config::Config;
 pub use engine::jce;
 pub use engine::pb;
+pub use engine::structs;
 pub use rq_engine as engine;
 pub use rq_engine::error::{RQError, RQResult};
 pub use rq_engine::hex;
