45c59bd93ac9d271e42d9ca77b833a39e17e21a8
niuhuan <niuhuancn@outlook.com>
2022-02-19 00:51:43 +0800

    Recursive protobuf

--- rq-engine/build.rs
+++ rq-engine/build.rs
@@ -1,35 +1,28 @@
 use std::io::Result;
+use std::path::Path;
+
+fn recursion<P: AsRef<Path>>(v: &mut Vec<String>, dir: P) -> Result<()> {
+    let rd = std::fs::read_dir(dir)?;
+    for x in rd {
+        let de = x?;
+        let path = de.path();
+        if path.is_dir() {
+            recursion(v, path)?;
+        } else {
+            let path = path.into_os_string().into_string().unwrap();
+            if path.ends_with(".proto") {
+                v.push(path);
+            }
+        }
+    };
+    Ok(())
+}
 
 fn main() -> Result<()> {
+    let mut v = Vec::<String>::new();
+    recursion(&mut v, "src/pb")?;
     prost_build::compile_protos(
-        &[
-            "src/pb/data.proto",
-            "src/pb/cmd0x388/cmd0x388.proto",
-            "src/pb/cmd0x3bb/cmd0x3bb.proto",
-            "src/pb/cmd0x6ff/smbcmd0x519.proto",
-            "src/pb/cmd0x6ff/subcmd0x501.proto",
-            "src/pb/msf/register_proxy.proto",
-            "src/pb/msg/head.proto",
-            "src/pb/msg/msg.proto",
-            "src/pb/msg/objmsg.proto",
-            "src/pb/msg/report.proto",
-            "src/pb/msg/TextMsgExt.proto",
-            "src/pb/msgtype0x210/subMsgType0x27.proto",
-            "src/pb/notify/group0x857.proto",
-            "src/pb/oidb/oidb.proto",
-            "src/pb/oidb/oidb0x8a7.proto",
-            "src/pb/oidb/oidb0x8fc.proto",
-            "src/pb/oidb/oidb0x88d.proto",
-            "src/pb/oidb/oidb0x758.proto",
-            "src/pb/oidb/oidb0x769.proto",
-            "src/pb/oidb/oidb0x990.proto",
-            "src/pb/oidb/oidb0xb77.proto",
-            "src/pb/oidb/oidb0xe07.proto",
-            "src/pb/oidb/oidb0xeac.proto",
-            "src/pb/profilecard/busi.proto",
-            "src/pb/profilecard/gate.proto",
-            "src/pb/structmsg/structmsg.proto",
-        ],
+        &v,
         &["src/pb"],
     )?;
     Ok(())
