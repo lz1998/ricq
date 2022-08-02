0375fb43e384d21f5a0aa2331446cd52d5770993
kkocdko <kkocdko@163.com>
2022-08-02 11:39:32 +0800

    prune redundant code by macro

--- ricq-core/src/pb/mod.rs
+++ ricq-core/src/pb/mod.rs
@@ -2,74 +2,33 @@
 
 include!(concat!(env!("OUT_DIR"), "/pb.rs"));
 
-pub mod cmd0x346 {
-    include!(concat!(env!("OUT_DIR"), "/cmd0x346.rs"));
+macro_rules! add_includes {
+    ($( $name:ident ),*) => {
+        $(
+            pub mod $name {
+                include!(concat!(env!("OUT_DIR"), "/", stringify!($name), ".rs"));
+            }
+        )*
+    };
 }
 
-pub mod cmd0x352 {
-    include!(concat!(env!("OUT_DIR"), "/cmd0x352.rs"));
-}
-
-pub mod cmd0x388 {
-    include!(concat!(env!("OUT_DIR"), "/cmd0x388.rs"));
-}
-
-pub mod cmd0x3bb {
-    include!(concat!(env!("OUT_DIR"), "/cmd0x3bb.rs"));
-}
-
-pub mod cmd0x6ff {
-    include!(concat!(env!("OUT_DIR"), "/cmd0x6ff.rs"));
-}
-
-pub mod cmd0x899 {
-    include!(concat!(env!("OUT_DIR"), "/cmd0x899.rs"));
-}
-
-pub mod longmsg {
-    include!(concat!(env!("OUT_DIR"), "/longmsg.rs"));
-}
-
-pub mod msf {
-    include!(concat!(env!("OUT_DIR"), "/msf.rs"));
-}
-
-pub mod msg {
-    include!(concat!(env!("OUT_DIR"), "/msg.rs"));
-}
-
-pub mod msgtype0x210 {
-    include!(concat!(env!("OUT_DIR"), "/msgtype0x210.rs"));
-}
-
-pub mod multimsg {
-    include!(concat!(env!("OUT_DIR"), "/multimsg.rs"));
-}
-
-pub mod notify {
-    include!(concat!(env!("OUT_DIR"), "/notify.rs"));
-}
-
-pub mod oidb {
-    include!(concat!(env!("OUT_DIR"), "/oidb.rs"));
-}
-
-pub mod online_status {
-    include!(concat!(env!("OUT_DIR"), "/online_status.rs"));
-}
-
-pub mod profilecard {
-    include!(concat!(env!("OUT_DIR"), "/profilecard.rs"));
-}
-
-pub mod sig_act {
-    include!(concat!(env!("OUT_DIR"), "/sig_act.rs"));
-}
-
-pub mod structmsg {
-    include!(concat!(env!("OUT_DIR"), "/structmsg.rs"));
-}
-
-pub mod short_video {
-    include!(concat!(env!("OUT_DIR"), "/short_video.rs"));
-}
+add_includes!(
+    cmd0x346,
+    cmd0x352,
+    cmd0x388,
+    cmd0x3bb,
+    cmd0x6ff,
+    cmd0x899,
+    longmsg,
+    msf,
+    msg,
+    msgtype0x210,
+    multimsg,
+    notify,
+    oidb,
+    online_status,
+    profilecard,
+    sig_act,
+    structmsg,
+    short_video
+);
