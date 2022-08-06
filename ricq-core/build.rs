fbac8f4479ae0f6d46ef898375c7e5e55a62e27c
LaoLittle <93422095+LaoLittle@users.noreply.github.com>
2022-08-06 16:45:22 +0800

    优化自动生成：将代码生成在各自的文件夹中

--- ricq-core/build.rs
+++ ricq-core/build.rs
@@ -1,5 +1,6 @@
+use std::{env, fs};
 use std::io::Result;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn recursion(v: &mut Vec<String>, dir: impl AsRef<Path>) -> Result<()> {
     let rd = std::fs::read_dir(dir)?;
@@ -21,6 +22,41 @@ fn recursion(v: &mut Vec<String>, dir: impl AsRef<Path>) -> Result<()> {
 fn main() -> Result<()> {
     let mut v = Vec::<String>::new();
     recursion(&mut v, "src/pb")?;
-    prost_build::compile_protos(&v, &["src/pb"])?;
+
+    prost_build::Config::new().compile_protos(&v, &["src/pb"])?;
+    let dir = env::var_os("OUT_DIR").unwrap();
+    let dir = Path::new(&dir);
+
+    let mut out = PathBuf::new();
+    out.push("src");
+    out.push("pb");
+
+    for f in fs::read_dir(dir)? {
+        if let Ok(entry) = f {
+            let p = entry.path();
+
+            if p.is_file() {
+                let name = p.file_stem().unwrap();
+                match name.to_str() {
+                    Some("_") => {
+                        continue;
+                    },
+                    Some("pb") => {
+                        fs::copy(&p, "src/pb/pb.rs").expect("Cannot copy pb.rs");
+                        continue;
+                    },
+                    _ => {
+                    }
+                }
+                out.push(name);
+                out.push("mod.rs");
+                fs::copy(&p, &out).expect(&format!("Cannot copy file: {:?}", out));
+                out.pop();
+                out.pop();
+            }
+        }
+    };
+
+    //prost_build::compile_protos(&v, &["src/pb"])?;
     Ok(())
 }
