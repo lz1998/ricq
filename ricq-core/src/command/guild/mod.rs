cf3922d4fbe3b37ebe6dc99df1f04ff414f88c5d
LaoLittle <93422095+LaoLittle@users.noreply.github.com>
2022-08-06 20:05:24 +0800

    add api

--- ricq-core/src/command/guild/mod.rs
+++ ricq-core/src/command/guild/mod.rs
@@ -1,2 +1,19 @@
+pub mod builder;
+pub mod decoder;
 mod dynamic_msg;
-mod builder;
+
+#[derive(Clone, Debug, Default)]
+pub struct FirstViewResponse {
+    pub guild_count: u32,
+    pub self_tinyid: u64,
+    pub direct_message_switch: u32,
+    pub direct_message_guild_count: u32,
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct GuildUserProfile {
+    pub tiny_id: u64,
+    pub nickname: String,
+    pub avatar_url: String,
+    pub join_time: Option<i64>,
+}
