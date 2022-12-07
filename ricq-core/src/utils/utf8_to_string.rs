24ab823eb0ab07457b1f4859f53fc4b0a1622520
kkocdko <kkocdko@163.com>
2022-12-08 05:12:53 +0800

    Remove accidentally committed code

--- ricq-core/src/utils/utf8_to_string.rs
+++ ricq-core/src/utils/utf8_to_string.rs
@@ -5,11 +5,11 @@ pub fn utf8_to_string(buf: impl Into<Vec<u8>>) -> String {
     let buf: Vec<u8> = buf.into(); // for `bytes::Bytes`
     match std::str::from_utf8(&buf) {
         Ok(_) => unsafe {
-            println!("[utf8_to_string] pass");
+            // println!("[utf8_to_string] pass");
             String::from_utf8_unchecked(buf)
         },
         Err(_) => {
-            println!("[utf8_to_string] failback");
+            // println!("[utf8_to_string] failback");
             String::from_utf8_lossy(&buf).into_owned() // don't use `.to_string()`
         }
     }
